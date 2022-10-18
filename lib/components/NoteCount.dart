063877b3791fa7b03e496e69bcc5809665f43095
Robotics Club of BRAC University <112168274+RobuLab@users.noreply.github.com>
2022-10-19 02:30:13 +0600

    done1

--- lib/components/NoteCount.dart
+++ lib/components/NoteCount.dart
@@ -1,26 +1,99 @@
 import 'package:flutter/material.dart';
 
 class NoteCount extends StatefulWidget {
-  const NoteCount({Key? key}) : super(key: key);
+  // const NoteCount({Key? key, }) : super(key: key);
+  final String taka;
+  List<Map> notes = [
+    {"amount" : 500, "count" : 0 },
+    {"amount" : 100, "count" : 0 },
+    {"amount" : 50, "count" : 0 },
+    {"amount" : 20, "count" : 0 },
+    {"amount" : 10, "count" : 0 },
+    {"amount" : 5, "count" : 0 },
+    {"amount" : 2, "count" : 0 },
+    {"amount" : 1, "count" : 0 }
+
+  ];
+  NoteCount({required this.taka});
 
   @override
   State<NoteCount> createState() => _NoteCountState();
 }
 
 class _NoteCountState extends State<NoteCount> {
-  List <int> notes = [500,100,50,20,10,5,2,1];
+
+
+  void calculation(){
+    num takaInt = int.parse(widget.taka);
+    for(var i = 0; i < widget.notes.length; i++){
+      if(takaInt != 0){
+        widget.notes[i]["count"] = takaInt~/widget.notes[i]["amount"];
+        takaInt = takaInt % widget.notes[i]["amount"];
+        print(takaInt);
+      }
+
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Flexible(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: List.generate(notes.length, (index) {
-              return Text(notes[index].toString());
-            }),
-        ),
-      )
-    );
+    calculation();
+    var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+    List<Widget> left = [];
+    for (var i = 0; i <4 ; i++){
+      left.add(Container(
+        padding: const EdgeInsets.all(10),
+        child: Text(widget.notes[i]["amount"].toString()+" : "+widget.notes[i]["count"].toString()),
+      ));
+
+    }
+
+    List<Widget> right = [];
+    for (var i = 4; i <widget.notes.length ; i++) {
+      right.add(Container(
+        padding: const EdgeInsets.all(10),
+        child: Text(widget.notes[i]["amount"].toString() + " : " +
+            widget.notes[i]["count"].toString()),
+      ));
+    }
+    if(isPortrait){
+      return Container(
+          padding: const EdgeInsets.all(28),
+          child: Flexible(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(widget.notes.length, (index) {
+                return Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                      widget.notes[index]["amount"].toString()+" : "+widget.notes[index]["count"].toString()),
+                );
+              }),
+            ),
+          )
+      );
+    }else{
+      return Container(
+          padding: const EdgeInsets.all(28),
+          child: Flexible(
+            child: Row(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: left,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: right,
+                ),
+              ],
+            ),
+
+
+          )
+      );
+    }
+
   }
 }
--- lib/components/keypad.dart
+++ lib/components/keypad.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class Keypad extends StatefulWidget {
   final Function addDigit;
-
-  const Keypad({required this.addDigit});
+  final Function allClear;
+  const Keypad({required this.addDigit, required this.allClear});
 
   @override
   State<Keypad> createState() => _KeypadState();
@@ -17,31 +17,56 @@ class _KeypadState extends State<Keypad> {
         children: [
           Row(
             children:  [
-              ElevatedButton(onPressed: (){widget.addDigit("5");}, child: Text('1')),
-              ElevatedButton(onPressed: null, child: Text('2')),
-              ElevatedButton(onPressed: null, child: Text('3'))
+              ElevatedButton(onPressed: (){widget.addDigit("1");}, child: Text('1')),
+              SizedBox(width: 10),
+              ElevatedButton(onPressed: (){widget.addDigit("2");}, child: Text('2')),
+              SizedBox(width: 10),
+              ElevatedButton(onPressed: (){widget.addDigit("3");}, child: Text('3'))
             ],
           ),
           Row(
             children: [
-              ElevatedButton(onPressed: null, child: Text('4')),
-              ElevatedButton(onPressed: null, child: Text('5')),
-              ElevatedButton(onPressed: null, child: Text('6'))
+              ElevatedButton(onPressed: (){widget.addDigit("4");}, child: Text('4')),
+              SizedBox(width: 10),
+              ElevatedButton(onPressed: (){widget.addDigit("5");}, child: Text('5')),
+              SizedBox(width: 10),
+              ElevatedButton(onPressed: (){widget.addDigit("6");}, child: Text('6'))
             ],
           ),
           Row(
             children: [
-              ElevatedButton(onPressed: null, child: Text('7')),
-              ElevatedButton(onPressed: null, child: Text('8')),
-              ElevatedButton(onPressed: null, child: Text('9'))
+              ElevatedButton(onPressed: (){widget.addDigit("7");}, child: Text('7')),
+              SizedBox(width: 10),
+              ElevatedButton(onPressed: (){widget.addDigit("8");}, child: Text('8')),
+              SizedBox(width: 10),
+              ElevatedButton(onPressed: (){widget.addDigit("9");}, child: Text('9'))
             ],
           ),
-          Row(
-            children: [
-              ElevatedButton(onPressed: null, child: Text('0')),
-              ElevatedButton(onPressed: null, child: Text('clear')),
-              // ElevatedButton(onPressed: null, child: Text('9'))
-            ],
+          Container(
+            width: 210,
+            height: 35,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 9,
+                  child: Container(
+
+                      child: ElevatedButton(onPressed: (){widget.allClear();}, child: Text('Clear'))
+                  ),
+
+                ),
+                SizedBox(width: 10,),
+                Expanded(
+                  flex: 4,
+                  child:  Container(
+
+                      child: ElevatedButton(onPressed: (){widget.addDigit("0");}, child: Text('0'))
+                  ),
+
+                ),
+                // ElevatedButton(onPressed: null, child: Text('9'))
+              ],
+            ),
           )
         ],
       ),
